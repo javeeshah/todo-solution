195eabee1e64a14ef330306f088e61c32f6c33c5
Jawad Hussain <shussain@sgfleet.com>
2026-02-27 16:11:19 +1100

    Updated the http client to provideHttpClient

--- frontend/todo-ui/src/main.ts
+++ frontend/todo-ui/src/main.ts
@@ -2,14 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 // ensure HttpClient is available to standalone components
 const combinedConfig = {
   ...appConfig,
   providers: [
     ...(appConfig?.providers ?? []),
-    importProvidersFrom(HttpClientModule)
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
 
